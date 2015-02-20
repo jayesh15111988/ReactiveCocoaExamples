313d0a3ef055be038a8fe68e0be5faf7cfa619e4
Jayesh Kawli <j.kawli@gmail.com>
2015-02-20 17:39:34 -0500

    Added refinements in subscribe signal lifecycle methods

--- JKReactiveRACSignalLifecycleViewController.m
+++ JKReactiveRACSignalLifecycleViewController.m
@@ -9,10 +9,13 @@
 #import "JKReactiveRACSignalLifecycleViewController.h"
 
 @interface JKReactiveRACSignalLifecycleViewController ()
+
 @property (weak, nonatomic) IBOutlet UIButton *startLifeCycleButton;
 @property (weak, nonatomic) IBOutlet UILabel *successLabel;
 @property (weak, nonatomic) IBOutlet UILabel *errorLabel;
+@property (weak, nonatomic) IBOutlet UISwitch *operationSelectionSwitch;
 @property (weak, nonatomic) IBOutlet UILabel *completedLabel;
+@property (assign, nonatomic) BOOL toIncludeCompletionBlock;
 
 @end
 
@@ -21,11 +24,20 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"RAC Signal lifecycle demo";
+    self.toIncludeCompletionBlock = YES;
     [[self.startLifeCycleButton rac_signalForControlEvents:UIControlEventTouchUpInside] subscribeNext:^(id x) {
+        
+        self.successLabel.text = @"Success Block Output";
+        self.errorLabel.text = @"Error Block Output";
+        self.completedLabel.text = @"Completion Block Output";
+        
         RACSignal* complexSignal = [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
-            [subscriber sendNext:@"We completed this block with success"];
-            [subscriber sendError:[NSError errorWithDomain:@"ReactiveCocoa" code:100 userInfo:@{NSLocalizedDescriptionKey: @"Failed with an unknown error. Please try again"}]];
-            [subscriber sendCompleted];
+            if(self.toIncludeCompletionBlock) {
+                [subscriber sendCompleted];
+            } else {
+                [subscriber sendNext:@"We completed this block with success"];
+                [subscriber sendError:[NSError errorWithDomain:@"ReactiveCocoa" code:100 userInfo:@{NSLocalizedDescriptionKey: @"Failed with an unknown error. Please try again"}]];
+            }
             return nil;
         }];
         
@@ -40,6 +52,10 @@
         [complexSignal subscribeError:^(NSError *error) {
             self.errorLabel.text = [NSString stringWithFormat:@"Block failed with an error - %@", [error localizedDescription]];
         }];
+    }];
+    
+    [[self.operationSelectionSwitch rac_signalForControlEvents:UIControlEventValueChanged] subscribeNext:^(UISwitch* x) {
+        self.toIncludeCompletionBlock = x.isOn;
     }];
 }
 
